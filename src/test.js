45c79ba1ae58578d46fb9f55360007cc8fa65024
Selçuk İTMİŞ <slck87@hotmail.com>
2019-01-14 17:03:49 +0300

    first commit

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native'
+
+export default class test extends Component {
+  render() {
+    return (
+      <View><Text>test</Text></View>
+    );
+  }
+}
